c4595c77f11ce894e3816b26468ed64c68313d3e
Peter Kajan <sknitro@gmail.com>
2022-11-05 13:28:27 +0100

    rewrite
    rewrite for new design

--- YT_all_uploads_as_playlist.user.js
+++ YT_all_uploads_as_playlist.user.js
@@ -1,87 +1,74 @@
 // ==UserScript==
-// @name        Youtube uploads as playlist
+// @name        Youtube uploads as playlist-
 // @namespace   pk-scripts
-// @version     1.51
-// @date        2022-02-19
+// @version     2.1
 // @description Create playlist of all uploaded videos from user.
 // @author      pK
 // @copyright   pK 2022
-
-// @license MIT https://opensource.org/licenses/MIT
-// @match          *://*.youtube.com/*
-// @downloadURL https://github.com/pkajan/YT-playlist/raw/master/YT_all_uploads_as_playlist.user.js
-// @updateURL   https://github.com/pkajan/YT-playlist/raw/master/YT_all_uploads_as_playlist.user.js
+// @icon        https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @match       *://*.youtube.com/*
+// @downloadURL https://-github.com/pkajan/YT-playlist/raw/master/YT_all_uploads_as_playlist.user.js
+// @updateURL   https://-github.com/pkajan/YT-playlist/raw/master/YT_all_uploads_as_playlist.user.js
+// @grant       none
 // ==/UserScript==
 
-(function () {
-    'use strict';
+(function() {
+    function addButton(Buttontext,cleanedID){
+        var button = document.createElement("div");
+        button.className = "playlistcreate";
+        button.style.cssText = "color: #FFFFFF;";
+        button.style.cssText = "margin-left: 10px;";
+        button.style.cssText += "font-family: Roboto,Arial,sans-serif;";
+        button.style.cssText += "border-radius: 20px;";
+        button.style.cssText += "font-size: 14px;";
+        button.style.cssText += "line-height: 15px;";
+        button.style.cssText += "padding: 10px;";
+        button.style.cssText += "font-weight: 500;";
+        button.style.cssText += "background-color: #272727;";
+        button.style.cssText += "text-decoration: none;";
+        button.style.cssText += "display: inline-block;";
+        button.style.cssText += "text-align: center;";
+        button.style.cssText += "cursor: pointer;";
 
-    if (document.getElementById("polymer-app") || document.getElementById("masthead") || window.Polymer) {
-        setInterval(function () {
-            if (window.location.href.indexOf("watch?v=") < 0) {
-                return false;
-            }
-            if (document.getElementById("count") && document.getElementById("show-all-videos-playlist") === null) {
-                Addytpolymer();
-            }
-        }, 100);
+        button.innerHTML = Buttontext;
+        button.onclick = function() {
+            window.location.replace("https://www.youtube.com/playlist?list=UU" + cleanedID.substring(2, cleanedID.size));
+        }
+        document.getElementsByClassName("item style-scope ytd-watch-metadata")[0].appendChild(button);
     }
 
-    function Addytpolymer() {
-        var buttonDiv = document.createElement("span");
-        buttonDiv.style.width = "100%";
-        buttonDiv.id = "show-all-videos-playlist";
-        var addButton = document.createElement("a");
-        addButton.appendChild(document.createTextNode("📖 Create Playlist"));
-        addButton.style.width = "100%";
-        addButton.style.backgroundColor = "#15388c";
-        addButton.style.color = "white";
-        addButton.style.textAlign = "center";
-        addButton.style.padding = "5px 10px";
-        addButton.style.margin = "0px 10px";
-        addButton.style.fontSize = "14px";
-        addButton.style.border = "0";
-        addButton.style.cursor = "pointer";
-        addButton.style.borderRadius = "2px";
-        addButton.style.fontFamily = "Roboto, Arial, sans-serif";
-        addButton.style.textDecoration = "none";
-        addButton.href = returnPLALISTURL();
-        addButton.target = "_blank";
-        buttonDiv.appendChild(addButton);
-        var targetElement = document.querySelectorAll("[id='count']");
-        for (var i = 0; i < targetElement.length; i++) {
-            if (targetElement[i].className.indexOf("ytd-video-primary-info-renderer") > -1) {
-                targetElement[i].appendChild(buttonDiv);
-            }
-        }
-    }
-    function returnPLALISTURL() {
-        var regexik = /.*channel.[U][C]/gi;
-        var channelIDz = null;
-        var counter = 0;
-        var firstOne = null;
-        var infoClass = document.getElementsByClassName("yt-simple-endpoint style-scope yt-formatted-string");
-        for (var i = 0; i < infoClass.length; i++) {
-            if (infoClass[i].href.includes("/channel/")) {
-                if (counter == 0) {
-                    firstOne = infoClass[i].href.replace(regexik, '');
-                }
-                counter++;
-                channelIDz = infoClass[i].href.replace(regexik, ''); //last one is the right one, leave it in variable
-            }
-        }
+    function doTheThing(){
+        setTimeout(function() {
+            async function fetchHTML() {
+                const response = await fetch(window.location.href);
+                // waits until the request completes...
+                console.log(response.text().then(function (html) {
+                    // This is the HTML from our response as a text string
+                    var docHTML = html;
+                    var idlocation = docHTML.search("externalChannelId"); //find location of externalChannelId
+                    var extracted = docHTML.substring(idlocation,idlocation+60); // get only this part+-
 
-        if (window.location.href.indexOf("watch?v=") > -1) {
-            if (counter != 1) {
-                channelIDz = firstOne;
-            }
-            var finalURL = "https://www.youtube.com/playlist?list=UU" + channelIDz;
+                    var cleanedID = extracted.replace("externalChannelId\":\"", ""); //remove unnecesary part of string
+                    cleanedID = cleanedID.substring(0,cleanedID.search("\",")); //remove unnecesary part of string
 
-            if (channelIDz != null) {
-                console.log("channel ID: " + channelIDz);
-                console.log("channel URL: " + finalURL);
-                return finalURL;
+                    addButton("Playlist",cleanedID);
+                    console.log("channel ID: " + cleanedID);
+                    console.log("Playlist URL: https://www.youtube.com/playlist?list=UU" + cleanedID.substring(2, cleanedID.size));
+                }));
             }
-        }
+            fetchHTML();
+        }, 5000);
     }
+
+    var oldUrl = null;
+    setInterval(function() {
+        if(oldUrl != window.location.href){
+            oldUrl = window.location.href;
+            const element = document.getElementsByClassName('playlistcreate')[0];
+            element.remove(); // Removes the div
+            doTheThing();
+        }
+    }, 1000);
+
+    doTheThing();
 })();
